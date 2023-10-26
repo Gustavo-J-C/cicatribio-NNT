6174c119480996279c236512ecef1020f1bc241a
Gustavo Costa <gustavojsc9@gmail.com>
2023-10-26 11:33:35 -0300

    inicio de validacao de campos

--- Cicatribio/Controllers/CreatePatient/RegisterPatientViewController.swift
+++ Cicatribio/Controllers/CreatePatient/RegisterPatientViewController.swift
@@ -10,22 +10,29 @@ import UIKit
 class RegisterPatientViewController: UIViewController {
 
     static let identifier: String = "RegisterPatientViewController"
+    @IBOutlet weak var tableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        tableView.dataSource = self
+        tableView.delegate = self
         // Do any additional setup after loading the view.
     }
-    
 
-    /*
-    // MARK: - Navigation
+}
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+extension RegisterPatientViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 6
     }
-    */
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell: UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "cell\(indexPath.row + 1)", for: indexPath)
+        return cell
+    }
+    
+    
+}
 
+extension RegisterPatientViewController: UITableViewDelegate {
+    
 }
--- Cicatribio/Controllers/LoginViewController.swift
+++ Cicatribio/Controllers/LoginViewController.swift
@@ -24,28 +24,32 @@ class LoginViewController: UIViewController {
     }
 
     @IBAction func loginButtonPress(_ sender: UIButton) {
-        guard let email = emailTextField.text, let password = passwordTextField.text else {
-            return
-        }
+        guard let email = emailTextField.text, !email.isEmpty,
+                 let password = passwordTextField.text, !password.isEmpty else {
+               showToast(message: "Favor Preencher os campos", isSuccess: false)
+               return
+           }
         
         apiManager.login(email: email, password: password) { success, user in
             if success, let user = user {
-                
                 DispatchQueue.main.async {
+                    self.showToast(message: "Login com sucesso", isSuccess: true)
                     UserManager.shared.currentUser = user
                     self.performSegue(withIdentifier: "goToNext", sender: self)
                 }
             } else {
                 print("Erro no login")
+                DispatchQueue.main.async {
+                    self.showToast(message: "Erro ao realizar login", isSuccess: false)
+                }
                 // Trate o erro de login, exiba uma mensagem de erro, etc.
             }
         }
     }
     
-    
-    
     @IBAction func registerButtonPress(_ sender: UIButton) {
         self.performSegue(withIdentifier: "goToRegister", sender: self)
+//        showToast(message: "Favor Preencher os campos", isSuccess: false)
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
@@ -53,5 +57,39 @@ class LoginViewController: UIViewController {
 
         }
     }
+    
+}
 
+extension LoginViewController {
+    func showToast(message: String, isSuccess: Bool) {
+        if let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene,
+           let window = windowScene.windows.first {
+            
+            let toastLbl = UILabel()
+            toastLbl.text = message
+            toastLbl.textAlignment = .center
+            toastLbl.font = UIFont.systemFont(ofSize: 18)
+            toastLbl.textColor = UIColor.white
+            toastLbl.backgroundColor = isSuccess ? UIColor.green.withAlphaComponent(0.6) : UIColor.red.withAlphaComponent(0.6)
+            toastLbl.numberOfLines = 0
+            
+            let textSize = toastLbl.intrinsicContentSize
+            let labelHeight = (textSize.width / window.frame.width) * 30
+            let labelWidth = min(textSize.width, window.frame.width - 40)
+            let adjustedHeight = max(labelHeight, textSize.height + 20)
+            
+            toastLbl.frame = CGRect(x: 20, y: (window.frame.height - 90) - adjustedHeight, width: labelWidth + 20, height: adjustedHeight)
+            toastLbl.center.x = window.center.x
+            toastLbl.layer.cornerRadius = 10
+            toastLbl.layer.masksToBounds = true
+            
+            window.addSubview(toastLbl)
+            
+            UIView.animate(withDuration: 3.0, animations: {
+                toastLbl.alpha = 0
+            }) { (_) in
+                toastLbl.removeFromSuperview()
+            }
+        }
+    }
 }
